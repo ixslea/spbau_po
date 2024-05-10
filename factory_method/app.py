7cdd843083737348ee60f26dc60a153e07cc9280
ixslea <83837721+ixslea@users.noreply.github.com>
2024-05-11 00:30:36 +0300

    Update app.py

--- factory_method/app.py
+++ factory_method/app.py
@@ -1,21 +1,18 @@
-from flask import Flask, request, url_for, render_template
+from flask import Flask, render_template
 
-
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 """Homepage"""
-
-
-
-if __name__ == "__main__":
-    app = Flask(__name__, template_folder="templates", static_folder="static")
-    @app.route('/')
-    def root():
-        return render_template(
-            'index.html'
+@app.route('/')
+def root():
+    return render_template(
+        'index.html'
     )
-    @app.route('/about/')
-    def about_method():
-        return render_template(
-            'about.html'
+@app.route('/about/')
+def about_method():
+    return render_template(
+        'about.html'
     )
-    app.run(debug=True)
+
+
+
